bb699ac8c245fab49a4b2911721c1627a5329f4f
Mehmet Bajin <bajin.mehmet@gmail.com>
2016-10-22 23:09:43 -0700

    feat: 14.2

--- 14-2-nailing-planks.js
+++ 14-2-nailing-planks.js
@@ -3,6 +3,11 @@
 'use strict';
 
 /**
+ * @typedef {!Array<number, number>}
+ */
+let Plank;
+
+/**
  * Counts the minimum number of nails that allow a series of planks to be
  * nailed.
  *
@@ -15,127 +20,72 @@
  * @return {number} Minimum number of nails needed or -1 if unable to nail all.
  */
 function solution(a, b, c) {
-  const p = a
-    .map( (l, i) => [l, b[i]])
-    .sort((x, y) => x[0] - y[0])  // sort first by plank-start
-    .sort((x, y) => x[1] - y[1]); // then by plank-end
+  let min = -1;
 
-  a = p.map((x) => x[0]);
-  b = p.map((x) => x[1]);
+  const combined = a.map((a, i) => [a, b[i]]);
+  const nails = c;
 
-  const numPlanks = p.length;
-  const nailedPlankRange = {
-    lo: numPlanks - 1,
-    hi: 0
-  };
-
-  for (let i = 0, numNails = c.length; i < numNails; i++) {
-    const nail = c[i];
-    // console.log(`nail: ${nail}`);
-
-    const lo = binarySearch(b, nail, Direction.Left);
-    // console.log(`lo  : ${lo}`);
-    if (!inBounds(b, lo)) {
-      continue;
-    }
-
-    const hi = binarySearch(a, nail, Direction.Right);
-    // console.log(`hi  : ${hi}`);
-    if (!inBounds(a, hi)) {
-      continue;
-    }
-
-    nailedPlankRange.lo = Math.min(nailedPlankRange.lo, lo);
-    nailedPlankRange.hi = Math.max(nailedPlankRange.hi, hi);
-    if (nailedPlankRange.lo === 0 &&
-        nailedPlankRange.hi === numPlanks - 1) {
-      return i + 1;
-    }
-  }
-
-  return -1;
-}
-
-/**
- * Search/slide direction.
- *
- * @enum {number}
- */
-const Direction = {
-  Left: -1,
-  Right: 1
-};
-
-/**
- * Searches for a value in an array.
- *
- * @param {!Array<number>} a Array to search.
- * @param {number} x Value to find.
- * @param {!Direction} direction
- *     If .Left:
- *       - If x in a, returns index of leftmost x.
- *       - Otherwise, returns index of smallest value > x.
- *     If .Right:
- *       - If x in a, returns index of rightmost x.
- *       - Otherwise, returns index of greatest value < x.
- */
-function binarySearch(a, x, direction) {
   let lo = 0;
-  let hi = a.length - 1;
-  while (lo < hi) {
+  let hi = nails.length - 1;
+  while (lo <= hi) {
     let mid = Math.floor((lo + hi) / 2);
-    if (x === a[mid]) {
-      return slide(a, mid, direction);
-    } else if (x > a[mid]) {
+    if (canNailPlanks(combined.slice(), nails, mid + 1)) {
+      min = mid + 1;
+      hi = mid - 1;
+    } else {
       lo = mid + 1;
-    } else { //x < a[mid]
-      hi = mid;
     }
   }
-  if (a[lo] === x) {
-    return slide(a, lo, direction);
+
+  return min;
+}
+
+/**
+ * Determines if planks can be nailed with a given nail count.
+ *
+ * @param {!Array<!Plank>} planks
+ * @param {!Array<number>} nails
+ * @param {number} nailCount
+ * @return {boolean} True if so, false otherwise.
+ */
+function canNailPlanks(planks, nails, nailCount) {
+  for (let i = 0; i < nailCount; i++) {
+    while (markNailable(planks, nails[i]));
+    if (allMarked(planks)) {
+      return true;
+    }
   }
+  return false;
+}
 
-  // lo === hi
-  // x still not found
-
-  let insertionPoint = lo;
-  while (a[insertionPoint] < x) {
-    insertionPoint++;
-  }
-
-  if (direction === Direction.Left) {
-    return insertionPoint;
-  } else {
-    return insertionPoint - 1;
+/**
+ * Marks nailable planks.
+ *
+ * @param {!Array<!Plank>} planks
+ * @param {number} nail Position of nail.
+ */
+function markNailable(planks, nail) {
+  for (let i = 0, l = planks.length; i < l; i++) {
+    const plank = planks[i];
+    if (plank !== null && plank[0] <= nail && nail <= plank[1]) {
+      planks[i] = null;
+    }
   }
 }
 
 /**
- * Determines if an index is within array bounds.
+ * Determines if all planks have been marked.
  *
- * @param {!Array<number>} a
- * @param {number} i
- * @return {boolean}
+ * @param {!Array<!Plank>} planks
+ * @return {boolean} True if so, false otherwise.
  */
-function inBounds(a, i) {
-  return 0 <= i && i < a.length;
-}
-
-/**
- * Slides an index while the value is unchanged.
- *
- * @param {!Array<number>} a
- * @param {number} i
- * @param {!Direction} d
- * @return {number} Left/right-most index with the value at index i.
- */
-function slide(a, i, d) {
-  const x = a[i];
-  while (inBounds(a, i) && a[i] === x) {
-    i += d;
+function allMarked(planks) {
+  for (let i = 0, l = planks.length; i < l; i++) {
+    if (planks[i] !== null) {
+      return false;
+    }
   }
-  return i - d;
+  return true;
 }
 
 console.log(solution([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10, 2])); // 4
